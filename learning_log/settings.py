9ff4b6d082654f0827e318273e54e26580932501
Mateusz Glowinski <mglowinski93@gmail.com>
2018-11-18 09:58:17 -1000

    Adjustment for Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -139,7 +139,7 @@ if os.getcwd() == '/app':
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO', 'https')
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static')
